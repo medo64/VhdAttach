aa4803c793ff6d0c8d0179631a714105657b9b0b
Josip Medved <jmedved@jmedved.com>
2012-02-26 23:08:56 -0500

    Does not crash on opening settings with invalid file name.

--- Source/VhdAttach/ListViewVhdItem.cs
+++ Source/VhdAttach/ListViewVhdItem.cs
@@ -8,26 +8,38 @@ namespace VhdAttach {
         public ListViewVhdItem(string fileName) {
             this.FileName = fileName;
 
-            var file = new FileInfo(fileName);
-            base.Text = file.Name;
-
             try {
-                if (file.Exists) {
-                    if (file.IsReadOnly) {
-                        this.ToolTipText = "File is read-only." + Environment.NewLine + fileName;
-                        this.ImageIndex = 1;
+                var file = new FileInfo(fileName);
+                base.Text = file.Name;
+
+                try {
+                    if (file.Exists) {
+                        if (file.IsReadOnly) {
+                            this.ToolTipText = "File is read-only." + Environment.NewLine + fileName;
+                            this.ImageIndex = 1;
+                        } else {
+                            this.ToolTipText = fileName;
+                            this.ImageIndex = 0;
+                        }
                     } else {
-                        this.ToolTipText = fileName;
-                        this.ImageIndex = 0;
+                        this.ToolTipText = "File not found." + Environment.NewLine + fileName;
+                        this.ImageIndex = 2;
                     }
-                } else {
-                    this.ToolTipText = "File not found." + Environment.NewLine + fileName;
-                    this.ImageIndex = 2;
+                } catch (Exception ex) {
+                    this.ToolTipText = ex.Message + Environment.NewLine + fileName;
+                    this.ImageIndex = 3;
                 }
-            } catch (Exception ex) {
-                this.ToolTipText = ex.Message + Environment.NewLine + fileName;
+            } catch (ArgumentException) {
+                var segments = fileName.Split(new char[] { Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar }, StringSplitOptions.RemoveEmptyEntries);
+                if (segments.Length > 0) {
+                    base.Text = segments[segments.Length - 1];
+                } else {
+                    base.Text = fileName;
+                }
+                this.ToolTipText = "Cannot parse file name \"" + fileName + "\"";
                 this.ImageIndex = 3;
             }
+
         }
 
         public string FileName { get; private set; }
