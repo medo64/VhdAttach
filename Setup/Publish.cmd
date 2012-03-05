b00788fb111ccd78ad043e9b47028b7cdc96769a
Josip Medved <jmedved@jmedved.com>
2012-03-04 21:20:28 -0500

    Updated publish script.

--- Setup/Publish.cmd
+++ Setup/Publish.cmd
@@ -1,34 +1,72 @@
 @ECHO OFF
 
+SET            FILE_SETUP=".\VhdAttach.iss"
+SET         FILE_SOLUTION="..\Source\VhdAttach.sln"
+SET      FILES_EXECUTABLE="..\Binaries\VhdAttach.exe" "..\Binaries\VhdAttachService.exe"
+SET           FILES_OTHER="..\Binaries\ReadMe.txt"
+SET   CERTIFICATE_SUBJECT="Josip Medved"
+SET CERTIFICATE_TIMESTAMP=
+
+
+ECHO --- BUILD SOLUTION
+ECHO.
+
+RMDIR /Q /S "..\Binaries" 2> NUL
+"%PROGRAMFILES(X86)%\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe" /Build "Release" %FILE_SOLUTION%
+IF ERRORLEVEL 1 PAUSE && EXIT /B %ERRORLEVEL%
 
 ECHO.
-ECHO --- Sign executables
 
+
+ECHO --- SIGN SOLUTION
+ECHO.
+
+IF [%CERTIFICATE_TIMESTAMP%]==[] (
+    "\Tools\SignTool\signtool.exe" sign /s "My" /n %CERTIFICATE_SUBJECT% /v %FILES_EXECUTABLE%
+) ELSE (
+    "\Tools\SignTool\signtool.exe" sign /s "My" /n %CERTIFICATE_SUBJECT% /t %CERTIFICATE_TIMESTAMP% /v %FILES_EXECUTABLE%
+)
+IF ERRORLEVEL 1 PAUSE && EXIT /B %ERRORLEVEL%
 
 ECHO.
-ECHO --- Setup
+
+
+ECHO --- BUILD SETUP
+ECHO.
 
 RMDIR /Q /S ".\Temp" 2> NUL
-CALL "%PROGRAMFILES(x86)%\Inno Setup 5\iscc.exe" /O".\Temp" ".\VhdAttach.iss"
-IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
+CALL "%PROGRAMFILES(x86)%\Inno Setup 5\iscc.exe" /O".\Temp" %FILE_SETUP%
+IF ERRORLEVEL 1 PAUSE && EXIT /B %ERRORLEVEL%
 
-FOR /F %%I IN ('DIR ".\Temp\*.exe" /B') DO SET SETUPEXE=%%I
-ECHO Setup is in file %SETUPEXE%
-
-
-ECHO.
-ECHO --- Sign setup
-
+FOR /F %%I IN ('DIR ".\Temp\*.exe" /B') DO SET _SETUPEXE=%%I
+ECHO Setup is in file %_SETUPEXE%
 
 ECHO.
-ECHO --- Move to releases
+
+
+ECHO --- SIGN SETUP
+ECHO.
+
+IF [%CERTIFICATE_TIMESTAMP%]==[] (
+    "\Tools\SignTool\signtool.exe" sign /s "My" /n %CERTIFICATE_SUBJECT% /v ".\Temp\%_SETUPEXE%"
+) ELSE (
+    "\Tools\SignTool\signtool.exe" sign /s "My" /n %CERTIFICATE_SUBJECT% /t %CERTIFICATE_TIMESTAMP% /v ".\Temp\%_SETUPEXE%"
+)
+IF ERRORLEVEL 1 PAUSE && EXIT /B %ERRORLEVEL%
+
+ECHO.
+
+
+ECHO --- RELEASE
+ECHO.
 
 MOVE ".\Temp\*.*" "..\Releases\."
-IF ERRORLEVEL 1 EXIT /B %ERRORLEVEL%
+IF ERRORLEVEL 1 PAUSE && EXIT /B %ERRORLEVEL%
 RMDIR /Q /S ".\Temp"
 
-
 ECHO.
+
+
 ECHO.
 ECHO Done.
 ECHO.
