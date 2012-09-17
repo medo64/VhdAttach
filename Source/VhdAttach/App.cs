022fbb22e621c8a39deef26403436de58303ad08
Josip Medved <jmedved@jmedved.com>
2012-09-16 21:01:04 -0700

    Added upgrade dialog after unhandled exception.

--- Source/VhdAttach/App.cs
+++ Source/VhdAttach/App.cs
@@ -81,6 +81,7 @@ namespace VhdAttach {
         private static void UnhandledCatch_ThreadException(object sender, ThreadExceptionEventArgs e) {
 #if !DEBUG
             Medo.Diagnostics.ErrorReport.ShowDialog(null, e.Exception, new Uri("http://jmedved.com/feedback/"));
+            Medo.Services.Upgrade.ShowDialog(null, new Uri("http://jmedved.com/upgrade/"));
 #else
             throw e.Exception;
 #endif
