54c7f3ef592b1b0bea9de23b6187eb4fd2e2bcd6
Josip Medved <jmedved@jmedved.com>
2015-04-09 20:33:34 -0700

    New release.

--- Source/VhdAttach/Properties/SolutionInfo.cs
+++ Source/VhdAttach/Properties/SolutionInfo.cs
@@ -4,5 +4,5 @@ using System.Reflection;
 [assembly: AssemblyCompany("Josip Medved")]
 [assembly: AssemblyCopyright("Copyright (c) 2009 Josip Medved <jmedved@jmedved.com>")]
 
-[assembly: AssemblyVersion("0.00.*")]
-[assembly: AssemblyInformationalVersion("0.00")]
+[assembly: AssemblyVersion("4.01.*")]
+[assembly: AssemblyInformationalVersion("4.01")]
